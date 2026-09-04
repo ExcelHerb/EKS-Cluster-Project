d6711e15e8356e786de11ba87f3adb2efe85a680
Ex0177 <excellenceo.herbert@gmail.com>
2026-09-04 11:06:34 -0400

    Terraform EKS

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
+
+}
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "eks_project"
+
+}
